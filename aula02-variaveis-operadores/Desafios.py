ca905ea5fdb0055351836f8c6caaf5d095fd4f3e
manuelacespede-ctrl <manuela.cespede@aluno.senai.br>
2026-03-16 11:40:41 -0300

    aula 2 variáveis

---
+++ aula02-variaveis-operadores/Desafios.py
@@ -0,0 +1,18 @@
+# DESAFIO 1
+'''
+nome = input("Digite seu nome: ")
+print(f"Bem-vindo(a), {nome}")
+
+#DESAFIO 2
+
+dia = input("Digite o dia do seu nascimento: ")
+mes = input("Digite o mês do seu nascimento: ")
+ano = input("Digite o ano do seu nascimento: ")
+print(f"A sua data de nascimento é {dia}/{mes}/{ano}")
+'''
+
+#DESAFIO 3
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+result = num1 + num2
+print(f"O número {num1}+{num2}={result}")
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,30 @@
+print("Olá, mundo!")
+
+print(7+4)
+print(" 7 + 4")
+print("7" + "4") # ESTOU CONCATENANDO STRINGS
+
+#São comentários de uma linha no python
+'''
+Isso são
+comentários
+de múltiplas 
+linhas
+'''
+
+# VARIÁVEIS
+# a variável nome recebe "Alexandre" e guarda na memoria
+nome = "Alexandre"
+idade = 26
+peso = 70.2
+
+print (nome, idade, peso)
+print(f"Oiii, {nome}")
+
+# INPUT - FORMULÁRIO SIMPLES NO PROMPT DE COMANDO (CMD)
+
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+peso = input("Digite seu peso: ")
+
+print(nome, idade, peso)
