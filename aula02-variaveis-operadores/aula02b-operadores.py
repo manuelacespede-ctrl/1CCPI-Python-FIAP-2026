2efee1639c08392e1b5c560a7647ff29954f9e8e
manuelacespede-ctrl <manu.cespede.tech@gmail.com>
2026-03-30 11:42:55 -0300

    aula 03 - modulos, funções e condicionais

---
+++ aula02-variaveis-operadores/aula02b-operadores.py
@@ -0,0 +1,43 @@
+num1 = 5
+num2 = 2
+
+print(type(num1), type(num2))
+
+resultado_op = num1 + num2
+print(resultado_op, type (resultado_op))
+
+#OPERADORES DE ATRIBUIÇÃO
+num = 15
+print() #pular linha
+print(num)
+
+num = num + 2 # acumulando 2
+print(num)
+
+num += 2
+print (num)
+
+#OPERADORES RELACIONAIS
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+print(6 == 2)
+
+idade = 20
+print(idade >= 18)
+
+logado = True
+print(logado, type(logado))
+
+maior_idade = idade >= 18
+print(maior_idade, type(maior_idade))
+
+#STRINGS...
+nome1 = "Marcos"
+nome2 = "marcos"
+
+print(nome1.upper() == nome2.upper())
