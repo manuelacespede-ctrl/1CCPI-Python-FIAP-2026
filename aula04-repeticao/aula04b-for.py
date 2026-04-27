9a65eabcc424ae26967c9fc060a6519a8edd57ef
manuelacespede-ctrl <manu.cespede.tech@gmail.com>
2026-04-27 11:24:45 -0300

    exercicios de repeticao

---
+++ aula04-repeticao/aula04b-for.py
@@ -0,0 +1,16 @@
+# for count_music in range (3)
+#     print (f"Música {count_music}")
+#
+# # de 1 até 11, pulando de 2 em 2
+# for 1 in range(1, 12, 2):
+#     print(i)
+
+# # ATV. 3
+# qtd_musicas = int(input("Digite a qtd de musicas que vc tem na playlist (DB): "))
+#
+# for i in rannge(qtd_musicas):
+#     print (f"Música {i}")
+
+for i in range (0, 4):
+    for j in range (0, 3, 2):
+        print(f"i:{i}, j:{j}")
